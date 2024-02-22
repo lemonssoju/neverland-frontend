0a2a042589f77c1fa07cb676604c5c7b1c400367
SJ-Kwak <seojin7@ewhain.net>
2024-02-23 00:12:40 +0900

    #1 feat: 검색 이전 화면 구현

--- src/components/Home/FeedSearch.tsx
+++ src/components/Home/FeedSearch.tsx
@@ -6,10 +6,15 @@ import styled from 'styled-components/native';
 import SearchBar from '../common/SearchBar';
 import BackButton from '../common/BackButton';
 import { UserItem } from './FeedItem';
-import { LIGHTBLACK } from '../../styles/GlobalColor';
+import { LIGHTBLACK, MINT, WHITE } from '../../styles/GlobalColor';
+import { B16, R14, R16 } from '../../styles/GlobalText';
+import CloseIcon from '../../assets/common/Close.svg';
 
 const FeedSearch = ({ navigation }: StackScreenProps<HomeStackParams, 'FeedSearch'>) => {
   const [search, setSearch] = useState<string>('');
+  const recommendData = ['나팔바지', '지브리', '크리스마스', '디즈니', '핑클', '소녀시대', '상속자들', '동방신기'];
+  const [recentSearches, setRecentSearches] = useState<string[]>(['로맨스', '서울 사투리', '응답하라', '서태지와 아이들']);
+
   return (
     <SafeAreaView>
       <View style={{flexDirection: 'row', paddingBottom: 10, borderBottomWidth: 1, borderBottomColor: LIGHTBLACK}}>
@@ -21,8 +26,59 @@ const FeedSearch = ({ navigation }: StackScreenProps<HomeStackParams, 'FeedSearc
           style={{width: 330, marginLeft: 5}}
         />
       </View>
+      { search.length > 0 ? (
+        <></>
+      ) : (
+        <View style={{padding: 15}}>
+          <View>
+            <B16 style={{color: MINT}}>추천 검색어</B16>
+            <View style={{flexDirection: 'row', flexWrap: 'wrap', marginTop: 10}}>
+              {recommendData.map((item, index) => {
+                return (
+                  <RecommendItem key={index} onPressIn={() => setSearch(item)} pressed={item === search}>
+                    <R14>{item}</R14>
+                  </RecommendItem>
+                )
+              })}
+            </View>
+          </View>
+          <View style={{marginTop: 40}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 10}}>
+              <B16 style={{color: MINT}}>최근 검색어</B16>
+              <TouchableOpacity>
+                <R14 style={{color: MINT}}>전체삭제</R14>
+              </TouchableOpacity>
+            </View>
+            <FlatList
+              data={recentSearches}
+              renderItem={({item}: any) => {
+                return (
+                  <View style={{flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: WHITE, justifyContent: 'space-between', alignItems: 'center', paddingVertical: 8}}>
+                    <TouchableOpacity onPress={() => setSearch(item)}>
+                      <R14>{item}</R14>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setRecentSearches(recentSearches.filter((recent: string) => recent !== item))}>
+                      <CloseIcon width={18} height={18} />
+                    </TouchableOpacity>
+                  </View>
+                )
+              }}
+            />
+          </View>
+        </View>
+      )}
     </SafeAreaView>
   )
 }
+
+const RecommendItem = styled.TouchableOpacity<{ pressed: boolean }>`
+  background: ${props => props.pressed ? MINT : LIGHTBLACK};
+  border-radius: 16px;
+  padding: 10px 16px;
+  justify-content: center;
+  align-items: center;
+  margin-right: 8px;
+  margin-bottom: 8px;
+`
 
 export default FeedSearch;
--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,7 +1,8 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
 import { View, TextInputProps, TextStyle, TextInput, TouchableOpacity } from "react-native";
-import { LIGHTBLACK } from "../../styles/GlobalColor";
+import { LIGHTBLACK, WHITE } from "../../styles/GlobalColor";
 import SearchIcon from '../../assets/common/Search.svg';
+import CloseIcon from '../../assets/common/Close.svg';
 
 interface SearchBarProps extends TextInputProps {
   search: string;
@@ -17,6 +18,7 @@ export default function SearchBar({
   style,
   ...rest
 }: SearchBarProps) {
+  const [focus, setFocus] = useState<boolean>(false);
   return (
     <View style={{ justifyContent: 'center', ...style}}>
       <TextInput
@@ -26,11 +28,13 @@ export default function SearchBar({
         }}
         placeholder={placeholder}
         placeholderTextColor={'#BBBBBB'}
-        style={{padding: 10, backgroundColor: LIGHTBLACK, borderRadius: 10}}
+        style={{padding: 10, backgroundColor: LIGHTBLACK, borderRadius: 10, color: '#BBBBBB', fontWeight: '700'}}
+        onEndEditing={() => setFocus(false)}
+        onChange={() => setFocus(true)}
         {...rest}
       />
-      <TouchableOpacity style={{position: 'absolute', right: 10}} >
-        <SearchIcon width={24} height={24} />
+      <TouchableOpacity style={{position: 'absolute', right: 10}} onPress={() => {search.length > 0 && !focus ? setSearch('') : setSearch(search)}} >
+        {search.length > 0 && !focus ? <CloseIcon width={24} height={24} /> : <SearchIcon width={24} height={24} />}
       </TouchableOpacity>
     </View>
   );
