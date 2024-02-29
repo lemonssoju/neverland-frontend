0b1fb4e3eebf9c9e80b554f78f805411fdf06842
SJ-Kwak <seojin7@ewhain.net>
2024-02-29 17:47:56 +0900

    #25 feat: 프로필 사진첩 구현

--- src/components/Profile/ProfileHome.tsx
+++ src/components/Profile/ProfileHome.tsx
@@ -1,13 +1,27 @@
 import { useState, useEffect, useCallback } from 'react';
-import { Image, SafeAreaView, View, TouchableOpacity, FlatList } from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  Modal,
+  FlatList,
+  ImageBackground,
+  Pressable,
+} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { ProfileStackParams } from '../../pages/Profile';
 import YoutubePlayer from 'react-native-youtube-iframe';
+import styled from 'styled-components/native';
+import { LIGHTBLACK, WHITE } from '../../styles/GlobalColor';
+import { Asset } from 'react-native-image-picker';
+import PhotoButton from '../common/PhotoButton';
 import { B16, B14, B12 } from '../../styles/GlobalText';
 
 import MusicIcon from '../../assets/common/Music.svg';
 import MenuIcon from '../../assets/common/Menu.svg';
-import { LIGHTBLACK } from '../../styles/GlobalColor';
+import CloseIcon from '../../assets/common/Close.svg';
+import BottomButton from '../common/BottomButton';
 
 interface ProfileProps {
   nickname: string;
@@ -17,9 +31,16 @@ interface ProfileProps {
   musicUrl?: string;
   following: number;
   follower: number;
+  albums?: string[];
 }
 
-const ProfileSection = ({ profile, navigation }: { profile: ProfileProps, navigation: any }) => {
+const ProfileSection = ({
+  profile,
+  navigation,
+}: {
+  profile: ProfileProps;
+  navigation: any;
+}) => {
   const [follow, setFollow] = useState<boolean>(false);
   const [playing, setPlaying] = useState<boolean>(false);
   const onStateChange = useCallback((state: string) => {
@@ -40,16 +61,26 @@ const ProfileSection = ({ profile, navigation }: { profile: ProfileProps, naviga
 
   return (
     <>
-      <View style={{flexDirection: 'row', paddingHorizontal: 20}}>
-        <Image source={{uri: profile.rep_pic}} style={{width: 80, height: 80, borderRadius: 180, marginRight: 10}} />
-        <View style={{justifyContent: 'space-between', flex: 1, paddingVertical: 5}}>
+      <View style={{ flexDirection: 'row', paddingHorizontal: 20 }}>
+        <Image
+          source={{ uri: profile.rep_pic }}
+          style={{ width: 80, height: 80, borderRadius: 180, marginRight: 10 }}
+        />
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flex: 1,
+            paddingVertical: 5,
+          }}>
           <B16>{profile.nickname}</B16>
           <B14>{profile.introduction}</B14>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{ flexDirection: 'row' }}>
             <TouchableOpacity onPress={() => navigation.navigate('FollowList')}>
               <B12>팔로잉 {profile.following}</B12>
             </TouchableOpacity>
-            <TouchableOpacity style={{marginLeft: 10}} onPress={() => navigation.navigate('FollowList')}>
+            <TouchableOpacity
+              style={{ marginLeft: 10 }}
+              onPress={() => navigation.navigate('FollowList')}>
               <B12>팔로우 {profile.follower}</B12>
             </TouchableOpacity>
           </View>
@@ -74,21 +105,85 @@ const ProfileSection = ({ profile, navigation }: { profile: ProfileProps, naviga
       <TouchableOpacity
         onPress={() => setFollow(!follow)}
         style={{
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          paddingVertical: 5, 
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingVertical: 5,
           backgroundColor: LIGHTBLACK,
           marginHorizontal: 20,
           marginVertical: 10,
-          borderRadius: 12
+          borderRadius: 12,
         }}>
         <B16>{follow ? '팔로잉' : '팔로우'}</B16>
       </TouchableOpacity>
     </>
-  )
-}
+  );
+};
 
-const ProfileHome = ({ navigation }: StackScreenProps<ProfileStackParams, 'ProfileHome'>) => {
+const AlbumSection = ({ onPress }: { onPress: () => void }) => {
+  return (
+    <ImageBackground
+      source={require('../../assets/Album.png')}
+      style={{ width: '100%', height: '100%' }}
+      imageStyle={{ width: 390, height: 460 }}>
+      <TouchableOpacity
+        onPress={onPress}
+        style={{
+          position: 'absolute',
+          top: 5,
+          left: 52,
+          transform: [{ rotate: '4deg' }],
+        }}>
+        <Image
+          source={{ uri: 'https://i.ytimg.com/vi/PFsH2I7xeFA/hqdefault.jpg' }}
+          style={{ width: 130, height: 200 }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={onPress}
+        style={{
+          position: 'absolute',
+          top: 5,
+          right: 54,
+          transform: [{ rotate: '-1deg' }],
+        }}>
+        <Image
+          source={{ uri: 'https://i.ytimg.com/vi/PFsH2I7xeFA/hqdefault.jpg' }}
+          style={{ width: 130, height: 200 }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={onPress}
+        style={{
+          position: 'absolute',
+          top: 210,
+          left: 38,
+          transform: [{ rotate: '4deg' }],
+        }}>
+        <Image
+          source={{ uri: 'https://i.ytimg.com/vi/PFsH2I7xeFA/hqdefault.jpg' }}
+          style={{ width: 130, height: 200 }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={onPress}
+        style={{
+          position: 'absolute',
+          top: 210,
+          right: 50,
+          transform: [{ rotate: '-1deg' }],
+        }}>
+        <Image
+          source={{ uri: 'https://i.ytimg.com/vi/PFsH2I7xeFA/hqdefault.jpg' }}
+          style={{ width: 130, height: 200 }}
+        />
+      </TouchableOpacity>
+    </ImageBackground>
+  );
+};
+
+const ProfileHome = ({
+  navigation,
+}: StackScreenProps<ProfileStackParams, 'ProfileHome'>) => {
   const [profile, setProfile] = useState<ProfileProps>({
     nickname: '황은정',
     introduction: '너 ㄸĦ문øłl ㅁı쳐',
@@ -96,17 +191,81 @@ const ProfileHome = ({ navigation }: StackScreenProps<ProfileStackParams, 'Profi
     music: '미쓰에이 - Bad girl Good girl',
     musicUrl: 'https://youtu.be/8TeeJvcBdLA?si=yffEamC12OAFs7HQ',
     following: 14,
-    follower: 16
-  })
+    follower: 16,
+    albums: [],
+  });
+  const [albumVisible, setAlbumVisible] = useState<boolean>(false);
+  const [photo, setPhoto] = useState<Asset[]>([
+    {
+      fileName: '',
+      width: 0,
+      height: 0,
+      uri: '',
+    },
+  ]);
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <TouchableOpacity style={{alignSelf: 'flex-end', width: 40, height: 40, alignItems: 'center', justifyContent: 'center'}}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <TouchableOpacity
+        style={{
+          alignSelf: 'flex-end',
+          width: 40,
+          height: 40,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginRight: 8,
+        }}>
         <MenuIcon width={24} height={24} />
       </TouchableOpacity>
       <ProfileSection profile={profile} navigation={navigation} />
+      <AlbumSection onPress={() => setAlbumVisible(true)} />
+      <Modal visible={albumVisible} transparent>
+        <Pressable
+          style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+          onPress={() => setAlbumVisible(false)}
+        />
+        <View
+          style={{
+            backgroundColor: LIGHTBLACK,
+            position: 'absolute',
+            width: '95%',
+            height: 300,
+            top: 300,
+            alignSelf: 'center',
+            borderRadius: 24,
+            padding: 10,
+          }}>
+          <TouchableOpacity
+            onPress={() => setAlbumVisible(false)}
+            style={{
+              position: 'absolute',
+              right: 0,
+              width: 40,
+              height: 40,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <CloseIcon width={24} height={24} />
+          </TouchableOpacity>
+          <PhotoBox>
+            <PhotoButton photo={photo} setPhoto={setPhoto} />
+          </PhotoBox>
+          <BottomButton label="등록" onPress={() => setAlbumVisible(false)} />
+        </View>
+      </Modal>
     </SafeAreaView>
-  )
-}
+  );
+};
+
+const PhotoBox = styled.View`
+  margin: 10px;
+  border-radius: 8px;
+  border: 1px solid ${WHITE};
+  width: 130px;
+  height: 200px;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+`;
 
 export default ProfileHome;
