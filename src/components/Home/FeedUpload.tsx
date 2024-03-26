21288126453b5d6bd8e34185424c732df50e2ed7
SJ-Kwak <seojin7@ewhain.net>
2024-03-26 17:56:18 +0900

    #11 feat: 사진 DALL-E 옵션 추가

--- src/components/Home/FeedUpload.tsx
+++ src/components/Home/FeedUpload.tsx
@@ -1,5 +1,14 @@
 import { useState, useEffect } from 'react';
-import { Text, ScrollView, TextInput, SafeAreaView, View, TouchableOpacity, Pressable, Modal } from 'react-native';
+import {
+  Text,
+  ScrollView,
+  TextInput,
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  Pressable,
+  Modal,
+} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { HomeStackParams } from '../../pages/Home';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
@@ -10,10 +19,11 @@ import BottomButton from '../common/BottomButton';
 import PhotoButton from '../common/PhotoButton';
 import { Asset } from 'react-native-image-picker';
 import { CategoryModal } from '../common/BottomModal';
-import { BLACK, LIGHTBLACK, MINT, WHITE } from '../../styles/GlobalColor';
+import { BLACK, GRAY, LIGHTBLACK, MINT, WHITE } from '../../styles/GlobalColor';
 import { B14 } from '../../styles/GlobalText';
 import ArrowIcon from '../../assets/common/Arrow.svg';
 import LinkIcon from '../../assets/common/Link.svg';
+import DrawIcon from '../../assets/common/Draw.svg';
 
 export interface FeedProps {
   title: string;
@@ -25,7 +35,10 @@ export interface FeedProps {
   musicUrl?: string;
 }
 
-const FeedUpload = ({ navigation, route }: StackScreenProps<HomeStackParams, 'FeedUpload'>) => {
+const FeedUpload = ({
+  navigation,
+  route,
+}: StackScreenProps<HomeStackParams, 'FeedUpload'>) => {
   const [feed, setFeed] = useState<FeedProps>({
     title: '',
     subtitle: '',
@@ -38,95 +51,286 @@ const FeedUpload = ({ navigation, route }: StackScreenProps<HomeStackParams, 'Fe
   const [categories, setCategories] = useState<string[]>(['영화']);
   const [categoryVisible, setCategoryVisible] = useState<boolean>(false);
   const [musicVisible, setMusicVisible] = useState<boolean>(false);
-  const [photo, setPhoto] = useState<Asset[]>([{
-    fileName: '',
-    width: 0,
-    height: 0,
-    uri: ''
-  }]);
+  const [photo, setPhoto] = useState<Asset[]>([
+    {
+      fileName: '',
+      width: 0,
+      height: 0,
+      uri: '',
+    },
+  ]);
+  const [option, setOption] = useState<string>('화가');
+  const [drawVisible, setDrawVisible] = useState<boolean>(false);
+  const [drawPrompt, setDrawPrompt] = useState<string>('');
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: BLACK}}>
-      <CustomHeader label='작성하기' onClose={() => {navigation.goBack()}} />
+    <SafeAreaView style={{ flex: 1, backgroundColor: BLACK }}>
+      <CustomHeader
+        label="작성하기"
+        onClose={() => {
+          navigation.goBack();
+        }}
+      />
       <ScrollView showsVerticalScrollIndicator={false}>
+        <OptionContainer>
+          <OptionButton
+            style={{ borderColor: option === '화가' ? WHITE : 'transparent' }}
+            onPress={() => setOption('화가')}>
+            <B14 style={{ marginVertical: 10 }}>👩‍🎨 AI 화가</B14>
+            <View
+              style={{
+                width: '101.5%',
+                height: option === '화가' ? 0 : 1,
+                backgroundColor: WHITE,
+              }}
+            />
+          </OptionButton>
+          <OptionButton
+            style={{ borderColor: option === '사진' ? WHITE : 'transparent' }}
+            onPress={() => setOption('사진')}>
+            <B14 style={{ marginVertical: 10 }}>📷 직접 선택</B14>
+            <View
+              style={{
+                width: '101.5%',
+                height: option === '사진' ? 0 : 1,
+                backgroundColor: WHITE,
+              }}
+            />
+          </OptionButton>
+        </OptionContainer>
         <PhotoBox>
-          <PhotoButton photo={photo} setPhoto={setPhoto} />
+          {option === '화가' ? (
+            <>
+              <B14 style={{ marginBottom: 10 }}>
+                AI 화가가 그림을 그려드려요!
+              </B14>
+              <TouchableOpacity onPress={() => setDrawVisible(true)}>
+                <DrawIcon color={WHITE} />
+              </TouchableOpacity>
+            </>
+          ) : (
+            <PhotoButton
+              photo={photo}
+              setPhoto={setPhoto}
+              label="원하는 사진을 직접 등록하세요!"
+            />
+          )}
         </PhotoBox>
-        <Input
-          label='제목' 
-          value={feed.title} 
-          onChangeText={(title) => {setFeed({ ...feed, title: title})}} 
-          isRequired 
-          placeholder='제목을 작성해주세요' 
-        />
-        <Input
-          label='한 줄 소개' 
-          value={feed.subtitle} 
-          onChangeText={(subtitle) => {setFeed({ ...feed, subtitle: subtitle})}} 
-          isRequired 
-          placeholder='한 줄 소개를 작성해주세요' 
-        />
-        <View style={{paddingHorizontal: 20, marginTop: 5, marginBottom: 15, flexDirection: 'row'}}>
-          <B14>콘텐츠 선택 *</B14>
-          <DropDownButton onPress={() => setCategoryVisible(true)}>
-            <B14>{categories[0]}</B14>
-            <ArrowIcon color={WHITE} strokeWidth={2} />
-          </DropDownButton>
-        </View>
-        <Input
-          label='같이 들으면 좋은 음악' 
-          value={feed.music} 
-          onChangeText={(music) => {setFeed({ ...feed, music: music})}}
-          placeholder='가수 - 제목 형식으로 입력해주세요.'
-          description='우측 아이콘을 클릭해 유튜브 링크를 삽입해주세요.'
-        />
-        <TouchableOpacity style={{position: 'absolute', right: 20, top: 465}} onPress={() => setMusicVisible(true)}>
-          <LinkIcon />
-        </TouchableOpacity>
-        <View style={{paddingHorizontal: 20, paddingTop: 15}}>
-          <B14 style={{marginBottom: 10}}>내용 *</B14>
-          <TextInput
-            value={feed.content}
-            onChangeText={(content) => {setFeed({ ...feed, content: content})}}
-            style={{borderWidth: 1, borderColor: WHITE, borderRadius: 8, padding: 10, marginBottom: 20, height: 150, color: WHITE}}
-            multiline
+        <View style={{ width: '95%', alignSelf: 'center' }}>
+          <Input
+            label="제목"
+            value={feed.title}
+            onChangeText={title => {
+              setFeed({ ...feed, title: title });
+            }}
+            isRequired
+            placeholder="제목을 작성해주세요"
           />
-          <BottomButton label='등록' onPress={() => { navigation.goBack(); navigation.navigate('FeedDetail'); }} />
+          <Input
+            label="한 줄 소개"
+            value={feed.subtitle}
+            onChangeText={subtitle => {
+              setFeed({ ...feed, subtitle: subtitle });
+            }}
+            isRequired
+            placeholder="한 줄 소개를 작성해주세요"
+          />
+          <View
+            style={{
+              paddingHorizontal: 20,
+              marginTop: 5,
+              marginBottom: 15,
+              flexDirection: 'row',
+            }}>
+            <B14>콘텐츠 선택 *</B14>
+            <DropDownButton onPress={() => setCategoryVisible(true)}>
+              <B14>{categories[0]}</B14>
+              <ArrowIcon color={WHITE} strokeWidth={2} />
+            </DropDownButton>
+          </View>
+          <Input
+            label="같이 들으면 좋은 음악"
+            value={feed.music}
+            onChangeText={music => {
+              setFeed({ ...feed, music: music });
+            }}
+            placeholder="가수 - 제목 형식으로 입력해주세요."
+            description="우측 아이콘을 클릭해 유튜브 링크를 삽입해주세요."
+          />
+          <TouchableOpacity
+            style={{ position: 'absolute', right: 20, top: 465 }}
+            onPress={() => setMusicVisible(true)}>
+            <LinkIcon />
+          </TouchableOpacity>
+          <View style={{ paddingHorizontal: 20, paddingTop: 15 }}>
+            <B14 style={{ marginBottom: 10 }}>내용 *</B14>
+            <TextInput
+              value={feed.content}
+              onChangeText={content => {
+                setFeed({ ...feed, content: content });
+              }}
+              style={{
+                borderWidth: 1,
+                borderColor: WHITE,
+                borderRadius: 8,
+                padding: 10,
+                marginBottom: 20,
+                height: 150,
+                color: WHITE,
+              }}
+              multiline
+            />
+            <BottomButton
+              label="등록"
+              onPress={() => {
+                navigation.goBack();
+                navigation.navigate('FeedDetail');
+              }}
+            />
+          </View>
         </View>
       </ScrollView>
       <BottomSheetModalProvider>
-        <CategoryModal categories={categories} setCategories={setCategories} categoryVisible={categoryVisible} setCategoryVisible={setCategoryVisible} unique />
+        <CategoryModal
+          categories={categories}
+          setCategories={setCategories}
+          categoryVisible={categoryVisible}
+          setCategoryVisible={setCategoryVisible}
+          unique
+        />
       </BottomSheetModalProvider>
-      <Modal visible={musicVisible} transparent>
-        <Pressable style={{flex:1, backgroundColor:'rgba(0, 0, 0, 0.2)'}} onPress={() => setMusicVisible(false)} />
-        <View style={{backgroundColor: LIGHTBLACK, position: 'absolute', width: '80%', height: 150, top: 340, alignSelf: 'center', borderRadius: 24, paddingVertical: 15}}>
-          <Input
-            label='Youtube' 
-            value={feed.musicUrl} 
-            onChangeText={(musicUrl) => {setFeed({ ...feed, musicUrl: musicUrl})}}
-            placeholder='유튜브 링크를 삽입해주세요.'
+      <Modal visible={drawVisible} transparent>
+        <Pressable
+          style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+          onPress={() => setDrawVisible(false)}
+        />
+        <View
+          style={{
+            backgroundColor: LIGHTBLACK,
+            position: 'absolute',
+            width: '80%',
+            height: 250,
+            top: 300,
+            alignSelf: 'center',
+            borderRadius: 24,
+            paddingVertical: 15,
+            alignItems: 'center',
+          }}>
+          <B14 style={{ marginVertical: 10 }}>
+            DALL-E에게 원하는 그림을 요청해주세요!
+          </B14>
+          <TextInput
+            value={drawPrompt}
+            onChangeText={content => {
+              setDrawPrompt(content);
+            }}
+            style={{
+              borderWidth: 1,
+              borderColor: WHITE,
+              borderRadius: 8,
+              padding: 10,
+              marginTop: 10,
+              marginBottom: 20,
+              height: 100,
+              color: WHITE,
+              width: '90%',
+            }}
+            multiline
+            placeholder="90년대 서울 패션 어쩌구 저쩌구를 그려줘"
+            placeholderTextColor={GRAY}
           />
-          <View style={{flexDirection: 'row', paddingHorizontal: 20, justifyContent: 'space-between'}}>
-            <TouchableOpacity 
-              style={{paddingVertical: 15, paddingHorizontal: 52, backgroundColor: MINT, borderRadius: 12}} 
-              onPress={() => {
-                setFeed({ ...feed, musicUrl: ''}); setMusicVisible(false)
+          <View
+            style={{
+              flexDirection: 'row',
+              paddingHorizontal: 20,
+              justifyContent: 'space-between',
+              width: '100%',
+            }}>
+            <TouchableOpacity
+              style={{
+                paddingVertical: 15,
+                paddingHorizontal: 52,
+                backgroundColor: MINT,
+                borderRadius: 12,
               }}
-            >
-              <B14 style={{color: BLACK}}>취소</B14>
+              onPress={() => {
+                setDrawPrompt('');
+                setDrawVisible(false);
+              }}>
+              <B14 style={{ color: BLACK }}>취소</B14>
             </TouchableOpacity>
-            <TouchableOpacity 
-              style={{paddingVertical: 15, paddingHorizontal: 52, backgroundColor: MINT, borderRadius: 12}}
-              onPress={() => setMusicVisible(false)}
-            >
-              <B14 style={{color: BLACK}}>완료</B14>
+            <TouchableOpacity
+              style={{
+                paddingVertical: 15,
+                paddingHorizontal: 52,
+                backgroundColor: MINT,
+                borderRadius: 12,
+              }}
+              onPress={() => setDrawVisible(false)}>
+              <B14 style={{ color: BLACK }}>완료</B14>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      <Modal visible={musicVisible} transparent>
+        <Pressable
+          style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+          onPress={() => setMusicVisible(false)}
+        />
+        <View
+          style={{
+            backgroundColor: LIGHTBLACK,
+            position: 'absolute',
+            width: '80%',
+            height: 150,
+            top: 340,
+            alignSelf: 'center',
+            borderRadius: 24,
+            paddingVertical: 15,
+          }}>
+          <Input
+            label="Youtube"
+            value={feed.musicUrl}
+            onChangeText={musicUrl => {
+              setFeed({ ...feed, musicUrl: musicUrl });
+            }}
+            placeholder="유튜브 링크를 삽입해주세요."
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              paddingHorizontal: 20,
+              justifyContent: 'space-between',
+            }}>
+            <TouchableOpacity
+              style={{
+                paddingVertical: 15,
+                paddingHorizontal: 52,
+                backgroundColor: MINT,
+                borderRadius: 12,
+              }}
+              onPress={() => {
+                setFeed({ ...feed, musicUrl: '' });
+                setMusicVisible(false);
+              }}>
+              <B14 style={{ color: BLACK }}>취소</B14>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                paddingVertical: 15,
+                paddingHorizontal: 52,
+                backgroundColor: MINT,
+                borderRadius: 12,
+              }}
+              onPress={() => setMusicVisible(false)}>
+              <B14 style={{ color: BLACK }}>완료</B14>
             </TouchableOpacity>
           </View>
         </View>
       </Modal>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const DropDownButton = styled.TouchableOpacity`
   flex-direction: row;
@@ -137,17 +341,40 @@ const DropDownButton = styled.TouchableOpacity`
   border-bottom-color: ${WHITE};
   padding-bottom: 3px;
   margin-left: 20px;
-`
+`;
+
+const OptionContainer = styled.View`
+  width: 80%;
+  justify-content: space-between;
+  align-items: center;
+  align-self: center;
+  flex-direction: row;
+  margin-top: 10px;
+`;
+
+const OptionButton = styled.TouchableOpacity`
+  width: 50%;
+  align-items: center;
+  border-radius: 8px 8px 0px 0px;
+  border-top-width: 1px;
+  border-right-width: 1px;
+  border-left-width: 1px;
+  border-color: ${WHITE};
+`;
 
 const PhotoBox = styled.View`
-  margin: 15px 20px;
-  border-radius: 8px;
-  border: 1px solid ${WHITE};
+  margin-horizontal: 20px;
+  margin-bottom: 15px;
+  border-radius: 0px 0px 8px 8px;
+  border-left-width: 1px;
+  border-right-width: 1px;
+  border-bottom-width: 1px;
+  border-color: ${WHITE};
   width: 80%;
   height: 220px;
   justify-content: center;
   align-items: center;
   align-self: center;
-`
+`;
 
 export default FeedUpload;
