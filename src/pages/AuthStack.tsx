c61b0c068e9a27838058f68356cb4b7619145df2
SJ-Kwak <seojin7@ewhain.net>
2024-04-07 20:47:27 +0900

    #47 chore: 불필요한 화면 정리

--- src/pages/AuthStack.tsx
+++ src/pages/AuthStack.tsx
@@ -3,15 +3,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Start from '../components/Auth/Start';
 import Login from '../components/Auth/Login';
 import SignUp from '../components/Auth/SignUp';
-import Contents from '../components/Auth/Contents';
-import Preferences from '../components/Auth/Preferences';
 
 export type AuthStackParams = {
   Start: undefined;
   Login: undefined;
   SignUp: undefined;
-  // Contents: undefined;
-  // Preferences: { categories: string[] }
 }
 
 const Stack = createStackNavigator<AuthStackParams>();
@@ -25,8 +21,6 @@ const Auth = () => {
       <Stack.Screen name="Start" component={Start} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="SignUp" component={SignUp} />
-      {/* <Stack.Screen name="Contents" component={Contents} /> */}
-      {/* <Stack.Screen name="Preferences" component={Preferences} /> */}
     </Stack.Navigator>
   )
 }
