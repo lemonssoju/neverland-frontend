7e556b03555cc23cb422226cecf9dbb7199db80a
SJ-Kwak <seojin7@ewhain.net>
2024-04-09 10:21:59 +0900

    #47 feat: 피드 작성 구현

--- src/components/Group/FeedUpload.tsx
+++ src/components/Group/FeedUpload.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import {
   Text,
   ScrollView,
@@ -8,48 +8,80 @@ import {
   TouchableOpacity,
   Pressable,
   Modal,
+  FlatList,
+  Image,
 } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import styled from 'styled-components/native';
 import CustomHeader from '../common/CustomHeader';
 import Input from '../common/Input';
 import BottomButton from '../common/BottomButton';
 import PhotoButton from '../common/PhotoButton';
 import { Asset } from 'react-native-image-picker';
-import { CategoryModal } from '../common/BottomModal';
-import { BLACK, GRAY, MINT, WHITE } from '../../styles/GlobalColor';
-import { B14 } from '../../styles/GlobalText';
-import ArrowIcon from '../../assets/common/Arrow.svg';
+import {
+  BLACK,
+  GRAY,
+  LIGHTPURPLE,
+  MINT,
+  PURPLE,
+  WHITE,
+} from '../../styles/GlobalColor';
+import { Caption, Label, Title } from '../../styles/GlobalText';
+import InfoIcon from '../../assets/common/Info.svg';
+import PaintIcon from '../../assets/common/Paint.svg';
 import LinkIcon from '../../assets/common/Link.svg';
-import DrawIcon from '../../assets/common/Draw.svg';
-import { GroupStackParams } from '../../pages/Group/FeedStack';
+import CalendarIcon from '../../assets/common/Calendar.svg';
+import { FeedStackParams } from '../../pages/Group/FeedStack';
+import MonthPicker from 'react-native-month-year-picker';
+import moment from 'moment';
 
 export interface FeedProps {
   title: string;
-  subtitle: string;
+  date: Date;
+  location: string;
   content: string;
-  category: string;
-  rep_pic: string;
+  rep_pic?: string;
   music?: string;
   musicUrl?: string;
+  members: string[];
 }
+
+const data = [
+  {
+    rep_pic: 'https://i.ytimg.com/vi/PFsH2I7xeFA/hqdefault.jpg',
+    nickname: '피터팬',
+  },
+  {
+    rep_pic: 'https://i.ytimg.com/vi/PFsH2I7xeFA/hqdefault.jpg',
+    nickname: '황은정',
+  },
+  {
+    rep_pic: 'https://i.ytimg.com/vi/PFsH2I7xeFA/hqdefault.jpg',
+    nickname: '김예지',
+  },
+  {
+    rep_pic: 'https://i.ytimg.com/vi/PFsH2I7xeFA/hqdefault.jpg',
+    nickname: '울랄라',
+  },
+  {
+    rep_pic: 'https://i.ytimg.com/vi/PFsH2I7xeFA/hqdefault.jpg',
+    nickname: '푸하하',
+  },
+];
 
 const FeedUpload = ({
   navigation,
-  route,
-}: StackScreenProps<GroupStackParams, 'FeedUpload'>) => {
+}: StackScreenProps<FeedStackParams, 'FeedUpload'>) => {
   const [feed, setFeed] = useState<FeedProps>({
     title: '',
-    subtitle: '',
+    date: new Date(),
+    location: '',
     content: '',
-    category: '',
     rep_pic: '',
     music: '',
     musicUrl: '',
+    members: [],
   });
-  const [categories, setCategories] = useState<string[]>(['영화']);
-  const [categoryVisible, setCategoryVisible] = useState<boolean>(false);
   const [musicVisible, setMusicVisible] = useState<boolean>(false);
   const [photo, setPhoto] = useState<Asset[]>([
     {
@@ -59,127 +91,183 @@ const FeedUpload = ({
       uri: '',
     },
   ]);
-  const [option, setOption] = useState<string>('화가');
   const [drawVisible, setDrawVisible] = useState<boolean>(false);
   const [drawPrompt, setDrawPrompt] = useState<string>('');
+  const [show, setShow] = useState<boolean>(false);
+
+  const showPicker = useCallback((value: boolean) => setShow(value), []);
+
+  const onValueChange = useCallback(
+    (event: any, newDate: any) => {
+      const selectedDate = newDate || feed.date;
+      showPicker(false);
+      setFeed({ ...feed, date: selectedDate });
+    },
+    [feed.date, showPicker],
+  );
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: BLACK }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: WHITE }}>
       <CustomHeader
         label="작성하기"
         onClose={() => {
           navigation.goBack();
         }}
       />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <OptionContainer>
-          <OptionButton
-            style={{
-              backgroundColor: option === '화가' ? MINT : 'transparent',
-            }}
-            onPress={() => setOption('화가')}>
-            <B14
-              style={{
-                marginVertical: 8,
-                color: option === '화가' ? BLACK : WHITE,
-              }}>
-              👩‍🎨 AI 화가
-            </B14>
-          </OptionButton>
-          <OptionButton
-            style={{
-              backgroundColor: option === '사진' ? MINT : 'transparent',
-            }}
-            onPress={() => setOption('사진')}>
-            <B14
-              style={{
-                marginVertical: 8,
-                color: option === '사진' ? BLACK : WHITE,
-              }}>
-              📷 직접 선택
-            </B14>
-          </OptionButton>
-        </OptionContainer>
-        <PhotoBox>
-          {option === '화가' ? (
-            <>
-              <TouchableOpacity onPress={() => setDrawVisible(true)}>
-                <DrawIcon color={WHITE} />
+      <FlatList
+        data={data}
+        ListHeaderComponent={() => {
+          return (
+            <View>
+              <View style={{ alignItems: 'center', paddingVertical: 5 }}>
+                <PaintIcon style={{ marginVertical: 20 }} />
+                <Title>사진과 함께 추억 퍼즐을 생성해드려요</Title>
+                <Caption style={{ color: GRAY }}>
+                  사진이 없으시다면, 내용을 기반으로 AI 화가가 추억 퍼즐을
+                  그려드려요.{' '}
+                </Caption>
+              </View>
+              <PhotoBox>
+                <PhotoButton photo={photo} setPhoto={setPhoto} />
+              </PhotoBox>
+              <Input
+                label="제목"
+                value={feed.title}
+                onChangeText={title => {
+                  setFeed({ ...feed, title: title });
+                }}
+                isRequired
+                placeholder="제목을 작성해주세요"
+              />
+              <Input
+                value={moment(feed.date).format('YYYY년 MM월').toString()}
+                label="날짜"
+                isRequired
+              />
+              <TouchableOpacity
+                onPress={() => showPicker(true)}
+                style={{
+                  position: 'absolute',
+                  bottom: 433,
+                  right: 5,
+                  zIndex: 1,
+                }}>
+                <CalendarIcon />
               </TouchableOpacity>
-              <B14 style={{ marginTop: 15 }}>AI 화가가 그림을 그려드려요!</B14>
-            </>
-          ) : (
-            <PhotoButton
-              photo={photo}
-              setPhoto={setPhoto}
-              label="원하는 사진을 직접 등록하세요!"
-            />
-          )}
-        </PhotoBox>
-        <View style={{ width: '95%', alignSelf: 'center' }}>
-          <Input
-            label="제목"
-            value={feed.title}
-            onChangeText={title => {
-              setFeed({ ...feed, title: title });
-            }}
-            isRequired
-            placeholder="제목을 작성해주세요"
-          />
-          <Input
-            label="한 줄 소개"
-            value={feed.subtitle}
-            onChangeText={subtitle => {
-              setFeed({ ...feed, subtitle: subtitle });
-            }}
-            isRequired
-            placeholder="한 줄 소개를 작성해주세요"
-          />
-          <View
-            style={{
-              paddingHorizontal: 20,
-              marginTop: 5,
-              marginBottom: 15,
-              flexDirection: 'row',
-            }}>
-            <B14>콘텐츠 선택 *</B14>
-            <DropDownButton onPress={() => setCategoryVisible(true)}>
-              <B14>{categories[0]}</B14>
-              <ArrowIcon color={WHITE} strokeWidth={2} />
-            </DropDownButton>
-          </View>
-          <Input
-            label="같이 들으면 좋은 음악"
-            value={feed.music}
-            onChangeText={music => {
-              setFeed({ ...feed, music: music });
-            }}
-            placeholder="가수 - 제목 형식으로 입력해주세요."
-            description="우측 아이콘을 클릭해 유튜브 링크를 삽입해주세요."
-          />
-          <TouchableOpacity
-            style={{ position: 'absolute', right: 20, top: 210 }}
-            onPress={() => setMusicVisible(true)}>
-            <LinkIcon />
-          </TouchableOpacity>
-          <View style={{ paddingHorizontal: 20, paddingTop: 15 }}>
-            <B14 style={{ marginBottom: 10 }}>내용 *</B14>
-            <TextInput
-              value={feed.content}
-              onChangeText={content => {
-                setFeed({ ...feed, content: content });
-              }}
+              <Input
+                label="장소"
+                isRequired
+                value={feed.location}
+                placeholder="장소를 입력해주세요."
+              />
+              <Input
+                label="같이 들으면 좋은 음악"
+                value={feed.music}
+                onChangeText={music => {
+                  setFeed({ ...feed, music: music });
+                }}
+                placeholder="가수 - 제목 형식으로 입력해주세요."
+                description="우측 아이콘을 클릭해 유튜브 링크를 삽입해주세요."
+              />
+              <TouchableOpacity
+                style={{ position: 'absolute', right: 5, bottom: 263 }}
+                onPress={() => setMusicVisible(true)}>
+                <LinkIcon />
+              </TouchableOpacity>
+              <Label>내용 *</Label>
+              <TextInput
+                value={feed.content}
+                onChangeText={content => {
+                  setFeed({ ...feed, content: content });
+                }}
+                style={{
+                  borderWidth: 1,
+                  borderColor: GRAY,
+                  borderRadius: 2,
+                  padding: 10,
+                  marginBottom: 20,
+                  height: 150,
+                  color: WHITE,
+                }}
+                multiline
+              />
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Label>퍼즐러 * </Label>
+                <InfoIcon />
+              </View>
+            </View>
+          );
+        }}
+        style={{
+          paddingHorizontal: 25,
+        }}
+        renderItem={({ item, index }: { item: any; index: number }) => {
+          const { rep_pic, nickname } = item;
+          let isInvited = feed.members.includes(nickname);
+          let isLastItem = data.length - 1 === index;
+          return (
+            <View
               style={{
-                borderWidth: 1,
-                borderColor: WHITE,
-                borderRadius: 8,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                borderTopWidth: 1,
+                borderStartColor: GRAY,
+                borderStartWidth: 1,
+                borderEndWidth: 1,
+                borderEndColor: GRAY,
+                borderColor: GRAY,
+                borderBottomColor: GRAY,
+                borderBottomWidth: isLastItem ? 1 : 0,
+                borderTopLeftRadius: index === 0 ? 2 : 0,
+                borderTopRightRadius: index === 0 ? 2 : 0,
+                borderBottomLeftRadius: isLastItem ? 2 : 0,
+                borderBottomRightRadius: isLastItem ? 2 : 0,
                 padding: 10,
-                marginBottom: 20,
-                height: 150,
-                color: WHITE,
-              }}
-              multiline
-            />
+              }}>
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Image
+                  source={{ uri: rep_pic }}
+                  style={{ width: 30, height: 30, borderRadius: 180 }}
+                />
+                <Label style={{ marginLeft: 10 }}>{nickname}</Label>
+              </View>
+              <TouchableOpacity
+                onPress={() => {
+                  isInvited
+                    ? setFeed({
+                        ...feed,
+                        members: feed.members.filter(
+                          (member: string) => member !== nickname,
+                        ),
+                      })
+                    : setFeed({
+                        ...feed,
+                        members: [...feed.members, nickname],
+                      });
+                }}
+                style={{
+                  width: 80,
+                  height: 25,
+                  backgroundColor: isInvited ? LIGHTPURPLE : PURPLE,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 12,
+                }}>
+                <Caption
+                  style={{
+                    color: isInvited ? PURPLE : WHITE,
+                    fontWeight: '700',
+                  }}>
+                  {isInvited ? '추가 완료' : '추가'}
+                </Caption>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+        ListFooterComponentStyle={{ marginTop: 20 }}
+        ListFooterComponent={() => {
+          return (
             <BottomButton
               label="등록"
               onPress={() => {
@@ -187,91 +275,18 @@ const FeedUpload = ({
                 navigation.navigate('FeedDetail');
               }}
             />
-          </View>
-        </View>
-      </ScrollView>
-      <BottomSheetModalProvider>
-        <CategoryModal
-          categories={categories}
-          setCategories={setCategories}
-          categoryVisible={categoryVisible}
-          setCategoryVisible={setCategoryVisible}
-          unique
+          );
+        }}
+      />
+      {show && (
+        <MonthPicker
+          onChange={onValueChange}
+          value={feed.date}
+          minimumDate={new Date(1970, 1)}
+          maximumDate={new Date()}
+          locale="ko"
         />
-      </BottomSheetModalProvider>
-      <Modal visible={drawVisible} transparent>
-        <Pressable
-          style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
-          onPress={() => setDrawVisible(false)}
-        />
-        <View
-          style={{
-            backgroundColor: BLACK,
-            position: 'absolute',
-            width: '80%',
-            height: 250,
-            top: 300,
-            alignSelf: 'center',
-            borderRadius: 24,
-            paddingVertical: 15,
-            alignItems: 'center',
-          }}>
-          <B14 style={{ marginVertical: 10 }}>
-            DALL-E에게 원하는 그림을 요청해주세요!
-          </B14>
-          <TextInput
-            value={drawPrompt}
-            onChangeText={content => {
-              setDrawPrompt(content);
-            }}
-            style={{
-              borderWidth: 1,
-              borderColor: WHITE,
-              borderRadius: 8,
-              padding: 10,
-              marginTop: 10,
-              marginBottom: 20,
-              height: 100,
-              color: WHITE,
-              width: '90%',
-            }}
-            multiline
-            placeholder="90년대 서울 패션 어쩌구 저쩌구를 그려줘"
-            placeholderTextColor={GRAY}
-          />
-          <View
-            style={{
-              flexDirection: 'row',
-              paddingHorizontal: 20,
-              justifyContent: 'space-between',
-              width: '100%',
-            }}>
-            <TouchableOpacity
-              style={{
-                paddingVertical: 15,
-                paddingHorizontal: 52,
-                backgroundColor: MINT,
-                borderRadius: 12,
-              }}
-              onPress={() => {
-                setDrawPrompt('');
-                setDrawVisible(false);
-              }}>
-              <B14 style={{ color: BLACK }}>취소</B14>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                paddingVertical: 15,
-                paddingHorizontal: 52,
-                backgroundColor: MINT,
-                borderRadius: 12,
-              }}
-              onPress={() => setDrawVisible(false)}>
-              <B14 style={{ color: BLACK }}>완료</B14>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
+      )}
       <Modal visible={musicVisible} transparent>
         <Pressable
           style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
@@ -279,14 +294,14 @@ const FeedUpload = ({
         />
         <View
           style={{
-            backgroundColor: BLACK,
+            backgroundColor: LIGHTPURPLE,
             position: 'absolute',
             width: '80%',
-            height: 150,
+            height: 160,
             top: 340,
             alignSelf: 'center',
             borderRadius: 24,
-            paddingVertical: 15,
+            padding: 15,
           }}>
           <Input
             label="Youtube"
@@ -299,31 +314,30 @@ const FeedUpload = ({
           <View
             style={{
               flexDirection: 'row',
-              paddingHorizontal: 20,
               justifyContent: 'space-between',
             }}>
             <TouchableOpacity
               style={{
-                paddingVertical: 15,
-                paddingHorizontal: 52,
-                backgroundColor: MINT,
+                paddingVertical: 10,
+                paddingHorizontal: 55,
+                backgroundColor: PURPLE,
                 borderRadius: 12,
               }}
               onPress={() => {
                 setFeed({ ...feed, musicUrl: '' });
                 setMusicVisible(false);
               }}>
-              <B14 style={{ color: BLACK }}>취소</B14>
+              <Label style={{ color: WHITE }}>취소</Label>
             </TouchableOpacity>
             <TouchableOpacity
               style={{
-                paddingVertical: 15,
-                paddingHorizontal: 52,
-                backgroundColor: MINT,
+                paddingVertical: 10,
+                paddingHorizontal: 55,
+                backgroundColor: PURPLE,
                 borderRadius: 12,
               }}
               onPress={() => setMusicVisible(false)}>
-              <B14 style={{ color: BLACK }}>완료</B14>
+              <Label style={{ color: WHITE }}>완료</Label>
             </TouchableOpacity>
           </View>
         </View>
@@ -332,43 +346,12 @@ const FeedUpload = ({
   );
 };
 
-const DropDownButton = styled.TouchableOpacity`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: 130px;
-  border-bottom-width: 1px;
-  border-bottom-color: ${WHITE};
-  padding-bottom: 3px;
-  margin-left: 20px;
-`;
-
-const OptionContainer = styled.View`
-  width: 80%;
-  justify-content: center;
-  align-items: center;
-  align-self: center;
-  flex-direction: row;
-  margin-top: 10px;
-  margin-bottom: 2px;
-  background: ${BLACK};
-  border-radius: 8px;
-  padding-vertical: 5px;
-`;
-
-const OptionButton = styled.TouchableOpacity`
-  width: 48%;
-  align-items: center;
-  border-radius: 8px;
-`;
-
 const PhotoBox = styled.View`
-  margin-horizontal: 20px;
-  margin-bottom: 15px;
-  border-radius: 8px;
-  background: ${BLACK};
-  width: 80%;
-  height: 220px;
+  margin: 5px 0px;
+  border-radius: 2px;
+  border: 1px solid ${GRAY};
+  width: 100%;
+  height: 180px;
   justify-content: center;
   align-items: center;
   align-self: center;
